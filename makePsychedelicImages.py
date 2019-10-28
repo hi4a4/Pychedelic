1ce95178544fc73bb3598b781f881ff85e2e0394
hi4a4 <hanson4a4@gmail.com>
2019-10-28 15:50:19 -0700

    Added command line args to makePsychedelicImages.py

--- makePsychedelicImages.py
+++ makePsychedelicImages.py
@@ -1,21 +1,31 @@
+"""
+Usage:
+    makePsychedelicImages.py [options] <mp3file> <savepath> <prefix>
+
+Options:
+    -h --help          Show this screen.
+    --framestart NUM   First frame to save.
+    --framestop NUM    Last frame to save.
+"""
 import cv2
+from docopt import docopt
 
-
-def getVideoFrames(url):
-    cap = cv2.VideoCapture(url)
+def getVideoFrames(args):
+    cap = cv2.VideoCapture(args['<mp3file>'])
     fps = cap.get(cv2.CAP_PROP_FPS)
-    for i in range(3246):
-        cap.set(1, i*fps)
+    for i in range(int(args['--framestart']), int(args['--framestop'])):
+        cap.set(1, i)
         check, frame = cap.read()
         if check:
-            cv2.imwrite(str(i)+".png", frame)
+            cv2.imwrite(args['<savepath>']+'/'+args['<prefix>']+str(i)+".png", frame)
         else:
             print("Failed to read frame")
-        break
     cap.release()
 
-def main():
-    getVideoFrames('./psyVideo.mp4')
+def main(args):
+    getVideoFrames(args)
 
 if __name__ == "__main__":
-    main()
+    args = docopt(__doc__, version='Four.2.Zero')
+    main(args)
+
